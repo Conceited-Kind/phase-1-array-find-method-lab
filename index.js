449bf7409fdd0e34ef4946cb2a0106b559983d57
Allan Kinuva <allankinuva@gmail.com>
2025-03-21 13:24:12 +0300

    find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// Function to find the year the Denver Broncos won the Superbowl
+function superbowlWin(record) {
+  const winRecord = record.find(game => game.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+// Export the function for testing
+module.exports = superbowlWin;
